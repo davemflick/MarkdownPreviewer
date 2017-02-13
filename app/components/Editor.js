e907dd714a18da1331ea73d038d8bc1d7f673e79
davemflick <davemflick@yahoo.com>
2017-02-13 15:16:10 -0500

    text transfers

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -1,19 +1,54 @@
 var React = require('react');
-var Preview = require('./Preview');
 var Marked = require('marked');
+var PropTypes = React.PropTypes;
+
+Marked.setOptions({
+  renderer: new Marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+});
 
 
 var Editor = React.createClass({
+    translateMarkdown: function (innerText) {
+    return {
+      __html: Marked(innerText)
+    };
+  },
+  
+
+  getInitialState: function (){
+    var innerText = 'This is the __inner__ #text'
+    return {
+      text: innerText,
+       MarkedText: this.translateMarkdown(innerText)
+    }
+  },
+
+  handleTextChange: function(e){
+    var change = e.target.value
+    this.setState({
+      text: change,
+      MarkedText: this.translateMarkdown(change)
+    })
+  },
   
 	render: function() {
 		return (
       <div className='col-xs-10 col-xs-offset-1'>
 			 <div className='editorContainer col-xs-6'>
-			  <textarea id='editorInput' type='text'>
+			  <textarea id='editorInput' type='text' value={this.state.text} onChange={this.handleTextChange}>
 			 </textarea>
 			</div>
       <div className='col-xs-6'>
-       <Preview />
+       <div className='previewContainer' dangerouslySetInnerHTML={this.state.MarkedText}>
+      
+      </div>
       </div>
       </div>
 		)
---
+++ app/components/ExampleText.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+
+var ExampleText = React.createClass({
+	render: function(){
+		return (
+
+		)
+	}
+})
+
+module.exports = ExampleText;
